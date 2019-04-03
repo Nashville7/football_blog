1e2ac045354628ee01cfffe164f530d49d2b4310
Nashville7 <nashvillejames7@gmail.com>
2019-04-03 13:15:27 +0200

    grid layout

---
+++ routes/delete.js
@@ -0,0 +1,27 @@
+// Get before you delete
+
+var express = require('express');
+var router = express.Router();
+var Posts = require('../db.json');
+
+router.get('/', function(req, res, next) {
+    console.log(req.params.postId);
+
+    // Make a post request to our database
+    request({
+        url: 'http://localhost:3004/posts' + req.params.postId,
+        method: 'DELETE',
+    },
+
+    function(error, response, body) {
+        var data = {
+            title: "name of the title",
+            posts: Posts,
+            message: 'successfully deleted'
+          };
+
+          res.redirect('..');
+});
+    });
+
+    
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Posts = require('../db.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '' });
+  // res.render('index', { title: 'Express' });
+
+  var data = {
+    title: 'Footballistic',
+    posts: Posts,
+    message: false
+  };
+
+  res.render('index', data);
+  
 });
 
 module.exports = router;
+
+// This is our index :)
